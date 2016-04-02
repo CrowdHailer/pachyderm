57606273512a0b970e532931b946fdc9afabc986
CrowdHailer <peterhsaxton@gmail.com>
2016-04-02 16:34:03 +0100

    better abstracted dream coding

--- test/lottery_test.exs
+++ test/lottery_test.exs
@@ -7,26 +7,46 @@ end
 
 defmodule LotteryTest do
   use ExUnit.Case
-  doctest Lottery
+  doctest LotteryCorp
 
   test "the truth" do
     assert 1 + 1 == 2
   end
 
   test "Add player to game" do
-    new_lottery = Game.open
-    IO.inspect(new_lottery)
     state = []
-    events = Lottery.issue_command(state, %Command.CreateGame{name: "Big Prizes"})
+    [] = LotteryCorp.list_games(state)
+    {:ok, {events, _meta}} = LotteryCorp.new_game(state, name: "Big Prizes")
+    # To be replaced with an apply events function so the knowledge that it is an array is hidden
+    state = state ++ events
+    [game] = LotteryCorp.all_games(state)
+    "Big Prizes" = LotteryCorp.Game.name(game)
+    # assert on game
+
+    {:ok, {events, _meta}} = LotteryCorp.add_player(state, game: game, player: "Bob")
+    # If using actor shouldn't need to respond with transaction??
+    :accepted = LotteryCorp.add_player(actor, game: game, player: "Bob")
+    # {:denied, reason}
+    # Passing in the first state should be a config step.
+    # It should be possibe to have a globally registered process that represents
+    :accepted = LotteryCorp.add_player(game: game, player: "Bob")
+    # Second event creates no events
+    :accepted = LotteryCorp.add_player(game: game, player: "Bob")
+
+    :accepted = LotteryCorp.draw_winner(game: game)
+    {:failed, {reason, command}} = LotteryCorp.add_player(game: game, player: "Bob")
+
+    events = LotteryCorp.issue_command(state, %Command.CreateGame{name: "Big Prizes"})
     {events, meta} = {[
       # {entity, attribute, value, transaction, learnt/unlearnt}
+      # Probably not needed
       {1, :"lottery/game", 1, 1, true},
       {2, :"game/name", "Big Prizes", 1, true}
     ], :stuff}
 
     # Simply concatinate the list of events
-    state = Lottery.apply_events(state, events)
-    {events, meta} = Lottery.issue_command(state, %Command.AddPlayer{game: 2, player: "Bob"})
+    state = LotteryCorp.apply_events(state, events)
+    {events, meta} = LotteryCorp.issue_command(state, %Command.AddPlayer{game: 2, player: "Bob"})
     assert events == [
       {2, :"game/player", "Bob", 2, true}
     ]
@@ -34,11 +54,11 @@ defmodule LotteryTest do
     # Already added to the lottery
     # {:ok, transaction = {events, meta}}
     # {:error, %{command: c, error: ex}}
-    {events, meta} = Lottery.issue_command(state, %Command.AddPlayer{game: 2, player: "Bob"})
+    {events, meta} = LotteryCorp.issue_command(state, %Command.AddPlayer{game: 2, player: "Bob"})
     # Should be transaction 3
     assert events == []
 
-    {:ok, {events, meta}} = Lottery.issue_command(state, %Command.DrawWinner{game: 2})
+    {:ok, {events, meta}} = LotteryCorp.issue_command(state, %Command.DrawWinner{game: 2})
     assert events == [
       {2, :"game/winner", "Bob", 4, true}
     ]
