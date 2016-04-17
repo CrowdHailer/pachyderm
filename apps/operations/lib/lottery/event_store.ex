ac57dc84a7c730baed059e0f942e8fc5a73905c6
CrowdHailer <peterhsaxton@gmail.com>
2016-04-17 20:06:18 +0100

    add event store

--- apps/operations/lib/lottery/event_store.ex
+++ apps/operations/lib/lottery/event_store.ex
@@ -9,6 +9,14 @@ defmodule LotteryCorp.Operations.EventStore do
     GenServer.call(event_store, {:persist, channel, event})
   end
 
+  def follow(ledger, follower) do
+    GenServer.call(ledger, {:follow, follower})
+  end
+
+  def monitor(ledger, follower) do
+    GenServer.call(ledger, {:monitor, follower})
+  end
+
   ## SERVER CALLBACKS
 
   def init(:no_state) do
@@ -19,8 +27,17 @@ defmodule LotteryCorp.Operations.EventStore do
     count = count + 1
     entry = {count, channel, event}
     Enum.each(followers, fn (follower) ->
-      send follower, {:"$LedgerEntry", entry}
+      send follower, {:"$EntryPersisted", entry}
     end)
     {:reply, {:ok, count}, {count, log ++ [entry], followers}}
+  end
+  def handle_call({:follow, follower}, _from, {last, log, followers}) do
+    Enum.each(log, fn (entry) ->
+      send follower, {:"$EntryPersisted", entry}
+    end)
+    {:reply, {:ok, last}, {last, log, followers ++ [follower]}}
+  end
+  def handle_call({:monitor, follower}, _from, {last, log, followers}) do
+    {:reply, {:ok, last}, {last, log, followers ++ [follower]}}
   end
 end
--- apps/operations/test/lottery/event_store_test.exs
+++ apps/operations/test/lottery/event_store_test.exs
@@ -2,7 +2,7 @@ defmodule LotteryCorp.Operations.EventStoreTest do
   use ExUnit.Case
   alias LotteryCorp.Operations.EventStore
 
-
+  # FIXME only follow single channel
   # The channel will simply be the lottery id
   test "appending values " do
     {:ok, event_store} = EventStore.start_link
@@ -12,31 +12,31 @@ defmodule LotteryCorp.Operations.EventStoreTest do
     assert second > first
   end
 
-  # test "Will send history to new follower" do
-  #   {:ok, event_store} = EventStore.start_link
-  #   channel = :test
-  #   {:ok, first} = EventStore.persist(event_store, channel, change = :change)
-  #   EventStore.follow(event_store, channel, self)
-  #   assert_receive({_, {^first, ^channel, ^change}})
-  # end
-  #
-  # test "Will send new messages to follower" do
-  #   {:ok, event_store} = EventStore.start_link
-  #   channel = :test
-  #   EventStore.follow(event_store, channel, self)
-  #   {:ok, first} = EventStore.persist(event_store, channel, change_1 = :change_1)
-  #   assert_receive({_, {^first, ^channel, ^change_1}})
-  #   {:ok, second} = EventStore.persist(event_store, channel, change_2 = :change_2)
-  #   assert_receive({_, {^second, ^channel, ^change_2}})
-  # end
-  #
-  # test "Can observe to get new entries only" do
-  #   {:ok, event_store} = EventStore.start_link
-  #   channel = :test
-  #   {:ok, first} = EventStore.persist(event_store, channel, change_1 = :change_1)
-  #   EventStore.monitor(event_store, channel, self)
-  #   refute_receive({_, {^first, ^channel, ^change_1}})
-  #   {:ok, second} = EventStore.persist(event_store, channel, change_2 = :change_2)
-  #   assert_receive({_, {^second, ^channel, ^change_2}})
-  # end
+  test "Will send history to new follower" do
+    {:ok, event_store} = EventStore.start_link
+    channel = :test
+    {:ok, first} = EventStore.persist(event_store, channel, change = :change)
+    EventStore.follow(event_store, self)
+    assert_receive({_, {^first, ^channel, ^change}})
+  end
+
+  test "Will send new messages to follower" do
+    {:ok, event_store} = EventStore.start_link
+    channel = :test
+    EventStore.follow(event_store, self)
+    {:ok, first} = EventStore.persist(event_store, channel, change_1 = :change_1)
+    assert_receive({_, {^first, ^channel, ^change_1}})
+    {:ok, second} = EventStore.persist(event_store, channel, change_2 = :change_2)
+    assert_receive({_, {^second, ^channel, ^change_2}})
+  end
+
+  test "Can observe to get new entries only" do
+    {:ok, event_store} = EventStore.start_link
+    channel = :test
+    {:ok, first} = EventStore.persist(event_store, channel, change_1 = :change_1)
+    EventStore.monitor(event_store, self)
+    refute_receive({_, {^first, ^channel, ^change_1}})
+    {:ok, second} = EventStore.persist(event_store, channel, change_2 = :change_2)
+    assert_receive({_, {^second, ^channel, ^change_2}})
+  end
 end
