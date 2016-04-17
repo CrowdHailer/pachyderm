13a7fa97de32c17f5d3a6024469e8ed58d4b909e
CrowdHailer <peterhsaxton@gmail.com>
2016-04-17 20:30:33 +0100

    alias modules

--- apps/operations/lib/lottery.ex
+++ apps/operations/lib/lottery.ex
@@ -1,14 +1,16 @@
 defmodule LotteryCorp.Operations do
   use Application
 
+  alias LotteryCorp.Operations.{Game, EventStore}
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
       # Define workers and child supervisors to be supervised
-      worker(LotteryCorp.Operations.EventStore, [[name: LotteryCorp.Operations.EventStore]]),
-      worker(LotteryCorp.Operations.Game.Supervisor, [[name: LotteryCorp.Operations.Game.Supervisor]]),
-      worker(LotteryCorp.Operations.Game.Registry, [LotteryCorp.Operations.Game.Supervisor]),
+      worker(EventStore, [[name: EventStore]]),
+      worker(Game.Supervisor, [[name: Game.Supervisor]]),
+      worker(Game.Registry, [Game.Supervisor]),
     ]
 
     opts = [strategy: :one_for_one, name: LotteryCorp.Operations.Supervisor]
@@ -17,28 +19,28 @@ defmodule LotteryCorp.Operations do
 
   def create_game do
     ref = generate_game_key
-    LotteryCorp.Operations.Game.Registry.lookup(LotteryCorp.Operations.Game.Registry, ref)
+    Game.Registry.lookup(Game.Registry, ref)
     {:ok, ref}
   end
 
   def add_player(ref, player) do
-    {:ok, game} = LotteryCorp.Operations.Game.Registry.lookup(LotteryCorp.Operations.Game.Registry, ref)
-    LotteryCorp.Operations.Game.add_player(game, player)
+    {:ok, game} = Game.Registry.lookup(Game.Registry, ref)
+    Game.add_player(game, player)
   end
 
   def remove_player(ref, player) do
-    {:ok, game} = LotteryCorp.Operations.Game.Registry.lookup(LotteryCorp.Operations.Game.Registry, ref)
-    LotteryCorp.Operations.Game.remove_player(game, player)
+    {:ok, game} = Game.Registry.lookup(Game.Registry, ref)
+    Game.remove_player(game, player)
   end
 
   def pick_winner(ref) do
-    {:ok, game} = LotteryCorp.Operations.Game.Registry.lookup(LotteryCorp.Operations.Game.Registry, ref)
-    LotteryCorp.Operations.Game.pick_winner(game)
+    {:ok, game} = Game.Registry.lookup(Game.Registry, ref)
+    Game.pick_winner(game)
   end
 
   def get_game(id) do
-    {:ok, game} = LotteryCorp.Operations.Game.Registry.lookup(LotteryCorp.Operations.Game.Registry, id)
-    {:ok, state} = LotteryCorp.Operations.Game.get_state(game)
+    {:ok, game} = Game.Registry.lookup(Game.Registry, id)
+    {:ok, state} = Game.get_state(game)
   end
 
   # http://stackoverflow.com/questions/32001606/how-to-generate-a-random-url-safe-string-with-elixir
