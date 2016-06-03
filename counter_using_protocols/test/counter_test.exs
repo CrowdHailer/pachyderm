947846bcdef40e8273a096c533da085556b3771f
CrowdHailer <peter@goodlord.co>
2016-06-03 11:55:57 +0100

    namespace count events, commands and state

--- counter_using_protocols/test/counter_test.exs
+++ counter_using_protocols/test/counter_test.exs
@@ -3,52 +3,52 @@ defprotocol GenSourced do
   def handle_event(state, event)
 end
 
-defmodule IncCommand do
+defmodule Counter.Command.Increment do
   defstruct amount: 1
 end
-defmodule SuperChargeCommand do
+defmodule Counter.Command.SuperCharge do
   defstruct []
 end
-defmodule IncEvent do
+defmodule Counter.Event.Incremented do
   defstruct amount: 1
 end
-defmodule SuperChargedEvent do
+defmodule Counter.Event.SuperCharged do
   defstruct []
 end
 
-defmodule CounterNormal do
+defmodule Counter.State.Normal do
   defstruct total: 0
 end
 
-defimpl GenSourced, for: CounterNormal do
-  def handle_command(_state, %IncCommand{amount: amount}) do
-    [%IncEvent{amount: amount}]
+defimpl GenSourced, for: Counter.State.Normal do
+  def handle_command(_state, %Counter.Command.Increment{amount: amount}) do
+    [%Counter.Event.Incremented{amount: amount}]
   end
-  def handle_command(state, %SuperChargeCommand{}) do
-    [%SuperChargedEvent{}]
+  def handle_command(state, %Counter.Command.SuperCharge{}) do
+    [%Counter.Event.SuperCharged{}]
   end
 
-  def handle_event(state = %{total: total}, %IncEvent{amount: amount}) do
+  def handle_event(state = %{total: total}, %Counter.Event.Incremented{amount: amount}) do
     %{state | total: total + amount}
   end
-  def handle_event(state, %SuperChargedEvent{}) do
-    %{state | :__struct__ => CounterSuperCharged}
+  def handle_event(state, %Counter.Event.SuperCharged{}) do
+    %{state | :__struct__ => Counter.State.SuperCharged}
   end
 end
 
-defmodule CounterSuperCharged do
+defmodule Counter.State.SuperCharged do
   defstruct total: 0
 end
 
-defimpl GenSourced, for: CounterSuperCharged do
-  def handle_command(_state, %IncCommand{amount: amount}) do
-    [%IncEvent{amount: amount}, %IncEvent{amount: amount}]
+defimpl GenSourced, for: Counter.State.SuperCharged do
+  def handle_command(_state, %Counter.Command.Increment{amount: amount}) do
+    [%Counter.Event.Incremented{amount: amount}, %Counter.Event.Incremented{amount: amount}]
   end
-  def handle_command(state, %SuperChargeCommand{}) do
+  def handle_command(state, %Counter.Command.SuperCharge{}) do
     []
   end
 
-  def handle_event(state = %{total: total}, %IncEvent{amount: amount}) do
+  def handle_event(state = %{total: total}, %Counter.Event.Incremented{amount: amount}) do
     %{state | total: total + amount}
   end
 end
@@ -57,7 +57,7 @@ defmodule Counter do
   use GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, %CounterNormal{})
+    GenServer.start_link(__MODULE__, %Counter.State.Normal{})
   end
 
   def command(pid, command) do
@@ -80,25 +80,25 @@ defmodule CounterTest do
   use ExUnit.Case
 
   test "the truth" do
-    counter_state = %CounterNormal{}
+    counter_state = %Counter.State.Normal{}
     IO.inspect counter_state
-    events = GenSourced.handle_command(counter_state, %IncCommand{})
+    events = GenSourced.handle_command(counter_state, %Counter.Command.Increment{})
     IO.inspect(events)
-    events = GenSourced.handle_command(counter_state, %SuperChargeCommand{})
+    events = GenSourced.handle_command(counter_state, %Counter.Command.SuperCharge{})
     IO.inspect(events)
-    state = GenSourced.handle_event(counter_state, %IncEvent{amount: 3})
-    state = GenSourced.handle_event(state, %SuperChargedEvent{})
+    state = GenSourced.handle_event(counter_state, %Counter.Event.Incremented{amount: 3})
+    state = GenSourced.handle_event(state, %Counter.Event.SuperCharged{})
     IO.inspect state
 
     IO.inspect("Starting")
     {:ok, pid} = Counter.start_link()
-    {:ok, state} = Counter.command(pid, %IncCommand{})
+    {:ok, state} = Counter.command(pid, %Counter.Command.Increment{})
     IO.inspect(state)
-    {:ok, state} = Counter.command(pid, %SuperChargeCommand{})
+    {:ok, state} = Counter.command(pid, %Counter.Command.SuperCharge{})
     IO.inspect(state)
-    {:ok, state} = Counter.command(pid, %IncCommand{})
+    {:ok, state} = Counter.command(pid, %Counter.Command.Increment{})
     IO.inspect(state)
-    {:ok, state} = Counter.command(pid, %SuperChargeCommand{})
+    {:ok, state} = Counter.command(pid, %Counter.Command.SuperCharge{})
     IO.inspect(state)
   end
 end
